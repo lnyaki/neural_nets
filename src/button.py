dfa6f9486aab17edba90332e769f5a405b6d4950
loic <loic.nyaki@gmail.com>
2019-05-29 02:23:42 +0200

    Text button almost ok. Just need centering

--- src/button.py
+++ src/button.py
@@ -2,10 +2,13 @@ import pygame
 from interactiveElement import InteractiveElement
 
 class Button(InteractiveElement):
+	pygame.init()
+	font = pygame.font.Font('freesansbold.ttf',20)
 
 	def __init__(self,text, dimension,position=(0,0)):
 		super().__init__(position)
-		self.text 		= text
+		self.textValue 	= text
+		self.textObject = self.getTextObject(text,self.font)
 		self.dimension 	= dimension
 		self.rectangle 	= pygame.Rect(self.position[0], self.position[1],self.dimension[0],self.dimension[1])
 		self.container 	= None
@@ -25,9 +28,18 @@ class Button(InteractiveElement):
 	
 	def setPosition(self, position):
 		super().setPosition(position)
+		print("Set position :"+str(position))
+		print(str(self.rectangle))
 		self.rectangle = pygame.Rect(self.container.position[0]+position[0],self.container.position[1]+position[1],self.dimension[0],self.dimension[1])
+
+	def getTextObject(self,text,font):
+		textSurface = font.render(text,True,(255,255,255))
+		return textSurface
 
 	def draw(self, gameDisplay):
 		pygame.draw.rect(gameDisplay,(255,0,0),self.rectangle)
 		pygame.gfxdraw.rectangle(gameDisplay,self.rectangle,(255,255,255))
+		gameDisplay.blit(self.textObject,(self.rectangle.left,self.rectangle.top))
+		
+
 
